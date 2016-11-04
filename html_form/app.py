d9f5ea2bc07c4c5d492cc847dff7cc9014bfbf52
Arun Balachandran <balachandran.arun94@gmail.com>
2016-11-04 16:45:32 -0500

    Implemented login functionality

--- html_form/app.py
+++ html_form/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, request, render_template, json
+from flask import Flask, request, render_template, json, redirect
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
+from flask import session  # to stop unauthorized access
 
 app = Flask(__name__)
-
+app.secret_key = '<secret_key_here>'
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = '<enter_your_username>'
-app.config['MYSQL_DATABASE_PASSWORD'] = '<enter_your_password>'
-app.config['MYSQL_DATABASE_DB'] = '<enter_name_of_database>'
+app.config['MYSQL_DATABASE_USER'] = '<username_here>'
+app.config['MYSQL_DATABASE_PASSWORD'] = '<password_here>'
+app.config['MYSQL_DATABASE_DB'] = 'Bucketlist'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 conn = mysql.connect()
@@ -39,6 +40,49 @@ def signUp():
         return json.dumps({'message':'User created successfully !'})
     else:
         return json.dumps({'error':str(data[0])})
+
+@app.route('/showSignin')
+def showSignin():
+    return render_template('signin.html')
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+        # different from the cursor used for signup
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+        if (len(data) > 0):
+            if check_password_hash(str(data[0][3]), _password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+
+    except Exception as e:
+        return render_template('error.html', error = str(e))
+    # close the cursor that we created only for this database
+    finally:
+        cursor.close()
+        con.close()
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+    return render_template('userHome.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
